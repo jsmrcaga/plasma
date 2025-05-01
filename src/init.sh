c539b804a227d62a4e1b8e4d1d40576854a776c3
Jo Colina <jo@jocolina.com>
2025-05-01 08:48:17 +0200

    Plasma - Base: update init script to run scripts & shell-scripts from config and userland

--- src/init.sh
+++ src/init.sh
@@ -1,20 +1,38 @@
+#!/bin/bash
+
+set -e
+
+# Allow no files in directories
+shopt -s nullglob
+
 # Run pre-hooks
-for file in /plasma-pre-hooks.d/*.sh; do
+for file in /plasma/pre-hooks.d/*.sh; do
   bash "$file"
 done
 
-# Configure screen
-# -> Should have happened in Dockerfile
+for source in /plasma/pre-hooks.d/*.env.sh; do
+  source "$source"
+done
 
-# Configure Sunshine
-# -> Mount config from user-land using volumes
+# Run plasma init commands
+for file in /plasma/init.d/*.sh; do
+  bash "$file"
+done
 
 # Run post-hooks
-for file in /plasma-post-hooks.d/*.sh; do
+for file in /plasma/post-hooks.d/*.sh; do
   bash "$file"
 done
 
-# Start x server?
+for source in /plasma/post-hooks.d/*.env.sh; do
+  source "$source"
+done
+
+shopt -u nullglob
 
 # Start container
+# Start X server
+/usr/bin/Xorg &
+
+# Start steam and sunshine
 steam && sunshine
