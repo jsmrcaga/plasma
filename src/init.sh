28d1ed6b46f250dd7743f013e189d443608ae7ac
Jo Colina <jo@jocolina.com>
2025-05-04 09:13:15 +0200

    Plasma - Base: add necessary initialization processes and fix Xorg flags

--- src/init.sh
+++ src/init.sh
@@ -30,9 +30,43 @@ done
 
 shopt -u nullglob
 
-# Start container
+## Start container
+
+# Start dbus for X and Sunshine
+# TODO: run as user
+dbus-daemon --system
+
+# Input magic
+# TODO: run as root
+/lib/systemd/systemd-udevd --daemon
+
+# For debugging but not necessary for running sunshine:
+# udevadm monitor &
+# udevadm trigger &
+
 # Start X server
-/usr/bin/Xorg &
+# Credit for all flags to Josh5
+/usr/bin/Xorg \
+    -ac \
+    -noreset \
+    -novtswitch \
+    -sharevts \
+    +extension RANDR \
+    +extension RENDER \
+    +extension GLX \
+    +extension XVideo \
+    +extension DOUBLE-BUFFER \
+    +extension SECURITY \
+    +extension DAMAGE \
+    +extension X-Resource \
+    -extension XINERAMA -xinerama \
+    +extension Composite +extension COMPOSITE \
+    -dpms \
+    -s off \
+    -nolisten tcp \
+    -iglx \
+    -verbose \
+    vt7 "${DISPLAY:?}" &
 
 # Start sunshine
 sunshine
