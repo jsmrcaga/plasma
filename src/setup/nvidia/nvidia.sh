521a08619a062b5d1be22ec01105f10e94429f5a
Jo Colina <jo@jocolina.com>
2025-07-12 18:10:08 +0200

    Nvidia: log nvidia commands and install xcvt

--- src/setup/nvidia/nvidia.sh
+++ src/setup/nvidia/nvidia.sh
@@ -11,6 +11,8 @@ else
 fi
 
 # Install Nvidia dependencies
+echo "Installing NVIDIA dependencies..."
+
 apt-get update && apt-get install -y \
   linux-headers-amd64 \
   libglvnd-dev \
@@ -18,17 +20,24 @@ apt-get update && apt-get install -y \
   wget \
   pkg-config
 
+echo "Cleaning NVIDIA dependencies..."
 apt-get clean autoclean -y && \
   apt-get autoremove -y && \
   rm -rf /var/lib/apt/lists/* /var/tmp/*
 
 # Download driver
+echo "Downloading NVIDIA drivers: $NVIDIA_DRIVER_VERSION..."
+
 wget \
     -q --show-progress --progress=bar:force:noscroll \
     -O /tmp/NVIDIA_DRIVERS.run \
     http://download.nvidia.com/XFree86/Linux-x86_64/$NVIDIA_DRIVER_VERSION/NVIDIA-Linux-x86_64-$NVIDIA_DRIVER_VERSION.run
+# @todo: figure out how to install pro drivers
+# error occurred first with Scaleway
 
 chmod +x /tmp/NVIDIA_DRIVERS.run
+
+echo "Installing NVIDIA drivers: $NVIDIA_DRIVER_VERSION..."
 
 /tmp/NVIDIA_DRIVERS.run \
     --silent \
@@ -50,9 +59,10 @@ chmod +x /tmp/NVIDIA_DRIVERS.run
     1> /var/log/nvidia-setup.plasma.log \
     2> /var/log/nvidia-setup.plasma.error
 
-
+echo "Removing NVIDIA drivers..."
 rm /tmp/NVIDIA_DRIVERS.run
 
 # Patch nvidia library
 # Using source to run in the same process
+echo "Patching NVIDIA nvenc..."
 source "/plasma/setup/nvidia/nvidia-nvenc-nvfbc.patch.sh"
